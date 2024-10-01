744cc7e64a26b57c68b0a246852bd7596411f481
Sofia313131 <sofiatataurova90283@gmail.com>
2024-10-01 14:48:54 +0300

    Third task solved

--- main.py
+++ main.py
@@ -22,3 +22,51 @@ print(f"{WHITE} {END}             {WHITE} {END}" * COUNT)
 print(f"{WHITE}  {END}           {WHITE}  {END}" * COUNT)
 print(f" {WHITE}   {END}       {WHITE}   {END} " * COUNT)
 print(f"   {WHITE}         {END}   " * COUNT)
+
+for _ in range(SEP_COUNT):
+    print()
+
+
+X_LIMIT = 90
+Y_LIMIT = 15
+
+X_SIZE = 1
+Y_SIZE = 0.4
+
+X_STEP = 5
+
+NUM_PAD = 4
+
+field = [["-" for _ in range(X_LIMIT)] for _ in range(Y_LIMIT)]
+
+
+def function(arg):
+    return arg / 3
+
+
+for x in range(1000):
+    x *= X_LIMIT / 1000
+
+    y = function(x)
+
+    x = round(x * X_SIZE)
+    y = round(y * Y_SIZE)
+
+    if 0 <= x < X_LIMIT and 0 <= y < Y_LIMIT:
+        field[Y_LIMIT - round(y) - 1][round(x)] = f"{RED} {END}"
+
+
+for y in range(Y_LIMIT):
+    print(str(round((Y_LIMIT - y - 1) / Y_SIZE)).rjust(NUM_PAD), "|", "".join(field[y]))
+print(" " * (NUM_PAD + 1) + "|=", end="")
+for x in range(X_LIMIT):
+    if x % X_STEP == 0:
+        print("|", end="")
+    else:
+        print("=", end="")
+print()
+print(" " * (NUM_PAD + 3), end="")
+for x in range(X_LIMIT):
+    if x % X_STEP == 0:
+        print(str(round(x / X_SIZE)).ljust(X_STEP), end="")
+print()
